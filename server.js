9d66b5216eb1f179d14d0c56f03c13822c038e7f
eccegallery <91703890+eccegallery@users.noreply.github.com>
2022-01-25 15:57:49 -0600

    add git ignore and node modules

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql2');
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-// middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
       // Your MySQL username,
-      user: 'Hill',
+      user: 'root',
       // Your MySQL password
       password: 'Buckhead410!',
       database: 'election'
@@ -20,16 +20,11 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
