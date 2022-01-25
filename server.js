646dd97c9518d845eeb302b83c0ed5d169497c7e
eccegallery <91703890+eccegallery@users.noreply.github.com>
2022-01-25 15:04:48 -0600

    add connection to database

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 app.get('/', (req, res) => {
     res.json({
