a6da7a71560a0a34a93582cb8f1c99c704e798c3
eccegallery <hddowdy@ncsu.edu>
2022-01-26 21:05:14 -0600

    Update index.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const { append } = require('express/lib/response');
-const router = express.Router();
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
-router.use(require('./candidateRoutes'));
-router.use(require('./partyRoutes'));
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
-append.use('/api', apiRoutes);
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
-module.exports = router;
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
